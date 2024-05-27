f215acb6deab0c7d5615e8ae9e8b3b9b078a994c
mgrybel <grybel.m.m@gmail.com>
2024-05-27 22:46:09 +0200

    Added the Route class

---
+++ src/main/java/com/ecommerce/api/Route.java
@@ -0,0 +1,7 @@
+package com.ecommerce.api;
+
+public class Route {
+    public static final String BASE_PATH = "/v1";
+    public static final String LOGIN_PATH = "/auth/login";
+    public static final String PRODUCT_PATH = "/product/";
+}
