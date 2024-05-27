f9e3fca3fd25508308b7e92e8b0c1f34c14b4cf9
mgrybel <grybel.m.m@gmail.com>
2024-05-27 22:43:43 +0200

    Updated POJO classes

---
+++ src/main/java/com/ecommerce/pojo/User.java
@@ -0,0 +1,34 @@
+package com.ecommerce.pojo;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+
+public class User {
+    @JsonProperty("email")
+    private String email;
+
+    @JsonProperty("password")
+    private String password;
+
+    @JsonProperty("email")
+    public String getEmail() {
+        return email;
+    }
+
+    @JsonProperty("email")
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @JsonProperty("password")
+    public String getPassword() {
+        return password;
+    }
+
+    @JsonProperty("password")
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
